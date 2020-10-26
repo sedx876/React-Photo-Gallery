661e64b89b7409dd649e979080d452fadc86f25e
sedx876 <swatkins876@gmail.com>
2020-10-26 06:49:38 -0400

    added imagegrid.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm'
+import ImageGrid from './comps/ImageGrid';
 
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      { selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
